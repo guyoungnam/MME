64f9837f95942a33793e13c0145ec344c5bfeffa
eak12 <eak12@naver.com>
2019-07-30 10:17:29 +0900

    Hello2 insert

--- GitTest/src/com/controller/TestServlet.java
+++ GitTest/src/com/controller/TestServlet.java
@@ -15,6 +15,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("TestSevlet.doGet");
 		System.out.println("Hello1");
+		System.out.println("Hello2");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
